c890a8d6aaf2bcba2f1a441082eb2eb980a0387f
SkysAreRed <skysarered2@gmail.com>
2022-11-26 15:32:57 +0100

    not working

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-//const numberButtons = document.querySelectorAll('[data-number]')
-//const dataOperation = document.querySelectorAll('[data-operation]')
-//const equalsButton = document.querySelector('[data-equals]')
-//const deleteButton = document.querySelector('[data-delete]')
-//const allClearButton = document.querySelector('[data-all-clear]')
-//const dataOperandTextElement = document.querySelector('[data-operand]')
-
-let result;
 function add(a, b) {
     return a + b;
 }
@@ -25,17 +17,11 @@ function divide(a, b) {
 function operate(num1, num2) {
     switch(operator) {
         case 'add':
-            result = num1 + num2;
-            console.log(`${num1} + ${num2} = ${result}`);
-            break
             return add(num1, num2);
         case 'subtract':
             return subtract(num1, num2);
     }
 }
-
-
-//let operator;
 
 const num1 = parseFloat(prompt('Insert first number'));
 const num2 = parseFloat(prompt('Insert second number'));
