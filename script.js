7985ebd0a5d553852d772963ab553aeffaef1e1c
SkysAreRed <skysarered2@gmail.com>
2022-11-26 12:58:24 +0100

    removed func from index, add oper func js

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case 'add':
+            return add(num1, num2);
+        case 'subtract':
+            return subtract(num1, num2);
+    }
+}
+
