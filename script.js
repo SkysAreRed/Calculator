cb90ba45a220431cefc6e60cfd9f8c42ed5e8d3a
SkysAreRed <skysarered2@gmail.com>
2022-11-26 15:23:57 +0100

    console log calc failed

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//const numberButtons = document.querySelectorAll('[data-number]')
+//const dataOperation = document.querySelectorAll('[data-operation]')
+//const equalsButton = document.querySelector('[data-equals]')
+//const deleteButton = document.querySelector('[data-delete]')
+//const allClearButton = document.querySelector('[data-all-clear]')
+//const dataOperandTextElement = document.querySelector('[data-operand]')
+
+let result;
 function add(a, b) {
     return a + b;
 }
@@ -14,12 +22,22 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(num1, num2, operator) {
+function operate(num1, num2) {
     switch(operator) {
         case 'add':
+            result = num1 + num2;
+            console.log(`${num1} + ${num2} = ${result}`);
+            break
             return add(num1, num2);
         case 'subtract':
             return subtract(num1, num2);
     }
 }
 
+
+//let operator;
+
+const num1 = parseFloat(prompt('Insert first number'));
+const num2 = parseFloat(prompt('Insert second number'));
+const operator = prompt('Choose between Add or Subtract');
+operate()
